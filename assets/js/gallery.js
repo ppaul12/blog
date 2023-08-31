b184deed1bd137c83974a36f0bc3f447187ec6b5
PENG-AO <pengao1293@gmail.com>
2023-08-31 21:55:19 +0900

    style: change gallery event from popping a modal to opening a new tab

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,26 +1,8 @@
-// init gallery modal
-function initGalleryModal() {
+// init gallery event
+function initGalleryEvent() {
     window.addEventListener("load", () => {
         document.querySelectorAll(".gallery img").forEach((img, idx) => {
-            // create modal dialog
-            let button = document.createElement("div")
-            button.className = "close-btn"
-            button.setAttribute("data-target", `pic${idx + 1}`)
-            button.onclick = toggleModal
-            let header = document.createElement("header")
-            header.appendChild(button)
-            header.appendChild(img.nextElementSibling.cloneNode(true))
-            let article = document.createElement("article")
-            article.appendChild(header)
-            article.appendChild(img.cloneNode())
-            let modal = document.createElement("dialog")
-            modal.id = `pic${idx + 1}`
-            modal.appendChild(article)
-            // find gallery
-            document.querySelector(".gallery").appendChild(modal)
-            // set target info
-            img.setAttribute("data-target", `pic${idx + 1}`)
-            img.onclick = toggleModal
+            img.setAttribute('onclick', `location.href='${img.getAttribute('src')}'`)
         })
     })
 }

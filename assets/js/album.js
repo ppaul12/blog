a22b9021d17c3a4e1c55bb1f6a961e8139e6b792
PENG-AO <pengao1293@gmail.com>
2023-09-04 19:39:46 +0900

    feat: upgrade album page

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,39 +1,66 @@
 // init album
 function initAlbum() {
-    // choose photos randomly
-    const sample = (array, size=1) => {
-        size = Math.max(0, Math.min(size, array.length))
-        if (size == 0) return []
-        const idx = Math.floor(Math.random() * array.length)
-        const rest = [].concat(array.slice(0, idx), array.slice(idx + 1, array.length))
-        return [array[idx]].concat(sample(rest, size - 1))
+    const shuffled = (array) => {
+        let currentIdx = array.length
+        while (currentIdx > 0) {
+            const targetIdx = Math.floor(Math.random() * currentIdx)
+            currentIdx--
+            [array[currentIdx], array[targetIdx]] = [array[targetIdx], array[currentIdx]]
+        }
+        return array
     }
-    const shufflePics = (pics) => {
+    const next = (pics, idx = 0) => {
         const album = document.getElementById("album")
-        album.innerHTML = ""
-        sample(pics, 4).forEach(pic => {
-            const img = document.createElement("img")
-            img.src = pic.path
-            const info = document.createElement("p")
-            info.innerHTML = pic.info
-            info.onclick = () => { location.href = pic.url }
-            const item = document.createElement("div")
-            item.className = "item"
-            item.appendChild(img)
-            item.appendChild(info)
-            album.appendChild(item)
-        })
+        album.scrollIntoView({ behavior: "smooth" })
+        if (document.getElementById("album-button").hasAttribute("state-pause")) {
+            window.setTimeout(next, 1000, pics, idx)
+        } else {
+            // move out the existing elements
+            album.childNodes.forEach(elem => {
+                elem.className = "move-out"
+            })
+            // move in new elements
+            window.setTimeout(() => {
+                album.innerHTML = ""
+                const pic = pics[idx]
+                const img = document.createElement("img")
+                img.src = pic.path
+                img.onclick = () => { location.href = pic.path }
+                album.appendChild(img)
+                const info = document.createElement("p")
+                info.innerHTML = pic.info
+                info.onclick = () => { location.href = pic.url }
+                album.appendChild(info)
+                album.childNodes.forEach(elem => {
+                    elem.className = "move-in"
+                })
+            }, 1000)
+            // change elements automatically
+            window.setTimeout(next, 5000, pics, (idx + 1) % pics.length)
+        }
+    }
+    const pause = (event = null) => {
+        const button = document.getElementById("album-button")
+        button.setAttribute("state-pause", '')
+        button.innerHTML = `<i class="fa-solid fa-play faa-shake faa-slow"></i>`
+        button.onclick = resume
+    }
+    const resume = (event = null) => {
+        const button = document.getElementById("album-button")
+        button.removeAttribute("state-pause")
+        button.innerHTML = `<i class="fa-solid fa-pause faa-shake faa-slow"></i>`
+        button.onclick = pause
     }
     window.addEventListener("load", () => {
         // prepare random photos
         fetch("/assets/json/album.json").then(response => response.json()).then(data => {
-            shufflePics(data)
-            // add shuffle button
+            // add album button
             let button = document.createElement("button")
+            button.id = "album-button"
             button.className = "contrast faa-parent animated-hover"
-            button.onclick = (event) => { shufflePics(data) }
-            button.innerHTML = `<i class="fa-solid fa-shuffle faa-shake faa-slow"></i>`
             document.querySelector(".button-box").appendChild(button)
+            // start slide show
+            resume(next(shuffled(data)))
         })
     })
 }
